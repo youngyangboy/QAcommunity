99681b5baba0209a9838879492ace54b138907a9
Yang <yangboy9711@gmail.com>
2022-04-19 12:32:31 +0800

    修复评论时的通知问题

--- src/main/java/work/ubox/community/service/CommentService.java
+++ src/main/java/work/ubox/community/service/CommentService.java
@@ -76,6 +76,7 @@ public class CommentService {
             if (question == null) {
                 throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
             }
+            comment.setCommentCount(0);
             commentMapper.insert(comment);
             question.setCommentCount(1);
             questionEXTMapper.incCommentCount(question);
@@ -87,6 +88,9 @@ public class CommentService {
     }
 
     private void createNotify(Comment comment, Long receiver, String notifierName, String outerTitle, NotificationTypeEnum notificationType, Long outerId) {
+        if(receiver==comment.getCommentator()){
+            return;
+        }
         Notification notification = new Notification();
         notification.setGmtCreate(System.currentTimeMillis());
         notification.setType(notificationType.getType());
